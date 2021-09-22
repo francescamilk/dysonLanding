af66a8aa5e59c665f470b8b31cae38723b1d781f
Francesca Santoriello <f.santoriello@hotmail.com>
2021-09-22 16:16:37 +0200

    faq js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@ import {responsiveNav} from './plugins/initResponsiveNav.js';
 import {prodColorPick} from './plugins/initProdColorPick.js';
 import {showFeatures} from './plugins/initShowFeatures.js';
 import {modalVideo} from './plugins/initModalVideo.js';
+import {openFaq} from './plugins/initOpenFaq.js';
 
 const app = () => {
    responsiveNav();
    prodColorPick();
    showFeatures();
    modalVideo();
+   openFaq();
 };
 
 app();
