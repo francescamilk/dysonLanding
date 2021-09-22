226a7ac2eff8606d7a531e65a3b6cdd1443f3259
Francesca Santoriello <f.santoriello@hotmail.com>
2021-09-22 19:15:15 +0200

    newsletter refine

--- js/plugins/initNewsletterPopup.js
+++ js/plugins/initNewsletterPopup.js
@@ -1,16 +1,21 @@
 const newsletterPopup = () => {
   const newsletter = document.getElementById('newsletterPopup');
+  const submit = document.getElementById('popSubmit');
   const close = document.getElementById('newsletterClose');
 
   window.onload = function() {
     setTimeout(function() {
       newsletter.classList.add('show');    
     }, 
-    15000 
+    10000 
     );
   };
 
   close.onclick = function() {
+    newsletter.classList.remove('show');
+  };
+
+  submit.onclick = function() {
     newsletter.classList.remove('show');
   };
 }
